faa917737418f0bf61b75a185c51c86cf9d24f78
dsmith111 <davidcsmith12@gmail.com>
2020-09-20 20:45:01 -0400

    Added some comments.

--- Homework/Homework2_AND.py
+++ Homework/Homework2_AND.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import copy
 
+
+# Method which uses the random library to return a weight value between -1 and 1.
 def initialize_weights():
     # Initialize Weights
     w = []
@@ -12,6 +14,7 @@ def initialize_weights():
         
     return w
 
+# The visualize guess method simply plots the data points, the target line and the guess line.
 def visualize_guess(w,finished):
 
     #Data to Classify
@@ -53,6 +56,7 @@ def visualize_guess(w,finished):
         print("x1,y1: {}  | x2,y2  {}".format([px1, py1], [px2, py2]))
     
 
+# The truth table method requires manual input of the x values. It is simply here to display the truth table to the user.
 def truth_table(y):
 
     #Truth Table
@@ -65,6 +69,8 @@ def truth_table(y):
     print("Truth Table for AND:\n {}\n".format(truth))
     return [x0, x1, x2]
 
+
+# The activation method returns the activation array of the input weights.
 def activation_function(w, x0, x1, x2):
     # Weight Check to Truth Table
 
@@ -73,16 +79,22 @@ def activation_function(w, x0, x1, x2):
   
     return [x, g]
 
+
+# The alter weights method decreases or increases the weights by the learning array dependent on which points were activated or not.
 def alter_weights(learning_rate,w,x, index, sign):
 
+    # Multiply the x inputs by the learning rate to generate a learning array.
     learning_array = np.multiply(x,learning_rate)
     
+    # If the sign of the incorrect activation element is supposed to be positive: increase, if it is supposed to be negative: decrease.
     if sign >= 0:
         w = np.add(np.array(w),learning_array[index])
     elif sign < 0:
         w = np.subtract(np.array(w),learning_array[index])
     return w
     
+# Method which takes the "logic array", an array telling if an element is on or off based on its sign, and compares the current with the guess
+# The method returns the index of an incorrect node and the sign it is supposed to be.
 def find_mismatch(y, logic_array_target, logic_array_guess,random_guess):
     temporary_array = list(range(len(logic_array_guess)))
     
@@ -100,7 +112,9 @@ def find_mismatch(y, logic_array_target, logic_array_guess,random_guess):
         if len(temporary_array) > 0:
             temporary_array.remove(selected_index)
         
-    
+
+# This is a reduced version of the pocket algorithm. It continuously cycles the weight values producing increasingly accurate...
+# values until it is able to correctly separate the nodes. It does not take into account the amount of error. 
 def pocket_algorithm(w, g, logic_array_target, learning_rate, x, x_list, y, time):
     count = 0
     pocket_w = w
@@ -142,6 +156,7 @@ def pocket_algorithm(w, g, logic_array_target, learning_rate, x, x_list, y, time
 
 # Execution Code Begins Here
 
+## This sets up our variables, prints our initial weights, runs the pocket algorithm, then prints the final weights and displays the chart.
 learning_rate = 0.01
 logic_array_target = [1, -1, -1, -1]
 output = [1, 0, 0, 0]
