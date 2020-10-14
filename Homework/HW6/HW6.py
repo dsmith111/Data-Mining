222f71589a3423bc5f9298508bbdbd0e77959752
dsmith111 <davidcsmith12@gmail.com>
2020-10-14 02:44:23 -0400

    HW6

--- Homework/HW6/HW6.py
+++ Homework/HW6/HW6.py
@@ -3,26 +3,28 @@ import numpy as np
 import random
 
 # Plot sine function, f(x)
-t = np.arange(0,1.1,0.1)
-y_sin = sin(2*np.pi*t)
-plt.plot(t, y_sin)
+t = np.arange(0,1,0.01)
+y_sin = np.sin(2*np.pi*t)
+plt.plot(t, y_sin, 'g')
 
 # Pick x uniformly in [0, 1]. Do it T times.
 
 T = 20
-x = np.array([[]*T])
+x = np.zeros([T, 2])
 for i in range(T):
-    x[i].append(random.random())
-    x[i].append(random.random())
+    x[i][0] = (random.random())
+    x[i][1] = (random.random())
 
-b = sin(2* np.pi*x)
+x = np.array(x)
+b = np.sin(2* np.pi*x)
+
 # For our estimate f_hat1, take the average height
-f1 = (b[:][0] + b[:][1])/2
+f1 =  x* + ((np.sum(b, axis = 1))/2)
 
 # Plot all thes estimate on one Plot
 
 px1 = 0
-px2 = 0
+px2 = 1
 
 for i in range(T):
     plt.plot([px1, px2], [f1[i], f1[i]], '-k')
@@ -30,7 +32,7 @@ for i in range(T):
 # Plot the average estime f1_bar
 f1_bar = np.mean(f1)
 
-plt.plot([px1, px2], [f1_bar, f1_bar], '-r', linewidth = 2 )
+plt.plot([px1, px2], [f1_bar, f1_bar], 'r' )
 
 # Find the bias
 f1_bias = y_sin - f1_bar
@@ -40,4 +42,6 @@ f1_rms_bias = np.sqrt(np.mean(f1_bias**2))
 
 # Square this for bias^2 in the book ex 2.8
 # Variance
-f1_var = np.mean((f1-1_bar)**2)
+f1_var = np.mean((f1_bar)**2)
+
+plt.show()
